afd5a23a3ee4565471bef7bb299374789a0195f1
tanghuiming <thm133@foxmail.com>
2018-11-01 18:32:06 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+  <dragElement></dragElement>
+  </div>
+</template>
+
+<script>
+import dragElement from './dragging-elements/'
+export default {
+  name: 'HelloWorld',
+  components:{
+    dragElement
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
